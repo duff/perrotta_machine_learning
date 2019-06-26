156a5b089039acd707305afbe59e79891050aca1
Duff OMelia <duff@omelia.org>
2019-06-26 08:21:15 -0400

    Remove inspect output

--- lib/pizza_reservations_gradient_descent.ex
+++ lib/pizza_reservations_gradient_descent.ex
@@ -4,9 +4,7 @@ defmodule PizzaReservationsGradientDescent do
   end
 
   def train(reservations, pizzas, iterations, learning_rate) do
-    Enum.reduce(1..iterations, 0, fn each, weight ->
-      IO.puts("Iteration #{each} -> #{loss(reservations, pizzas, weight)}")
-      IO.inspect(gradient(reservations, pizzas, weight), label: :gradient)
+    Enum.reduce(1..iterations, 0, fn _each, weight ->
       weight - gradient(reservations, pizzas, weight) * learning_rate
     end)
   end
@@ -22,21 +20,5 @@ defmodule PizzaReservationsGradientDescent do
 
   defp average(values) do
     Enum.sum(values) / length(values)
-  end
-
-  defp loss(reservations, pizzas, weight) do
-    losses(reservations, pizzas, weight)
-    |> average
-  end
-
-  defp predictions(reservations, weight) do
-    reservations
-    |> Enum.map(&predict(&1, weight, 0))
-  end
-
-  defp losses(reservations, pizzas, weight) do
-    predictions(reservations, weight)
-    |> Enum.zip(pizzas)
-    |> Enum.map(fn {prediction, actual} -> (prediction - actual) |> :math.pow(2) end)
   end
 end
